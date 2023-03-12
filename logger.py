fb0593822fe365fe2af1817a82c9e4e630645fce
Alexei <alexei@alexey.com>
2023-03-12 11:41:02 +0300

    new met3

--- logger.py
+++ logger.py
@@ -31,79 +31,61 @@ def filter_data():
             list_filter = filter_string.split(";")
         else:
             list_filter = [filter_string]
-
-        is_found = False
-        count_2 = 0
+        print(list_filter)
+    #     is_found = False
+    #     count_2 = 0
         for element in list_data:
-            temp_record = element.split(";")
-            # print(temp_record)
+            # print(element)
             count = 0
-            for record in temp_record:
-                for element_filter in list_filter:
-                    if element_filter.lower() in record.lower(): #and len(element_filter.lower()) == len(record.lower()):
-                        count += 1
-                        # print(count, record)
-            if count >= len(list_filter):
-                print(element)
-                count_2 += 1
-                is_found = True
-            # print(count_2)
-    if not is_found:
-        print("Записей не найдено")
-    else:
-        if count_2 > 1: 
-            lit = "о"
-            lit_2 = "а"
-        else:
-            lit = ""
-            lit_2 = ""
-        print(f'Найден{lit} {count_2} элемент{lit_2}')
+            for i in list_filter:
                 
-def change_data ():
-    filter_data()
+                if i in element:
+                    # print(i)
+                    count += 1
+            # print(count)
+            if count == len(list_filter):
+                print(element)
+                
+    #         temp_record = element.split(";")
+    #         # print(temp_record)
+    #         count = 0
+    #         for record in temp_record:
+    #             for element_filter in list_filter:
+    #                 if element_filter.lower() in record.lower(): #and len(element_filter.lower()) == len(record.lower()):
+    #                     count += 1
+    #                     # print(count, record)
+    #         if count >= len(list_filter):
+    #             print(element)
+    #             count_2 += 1
+    #             is_found = True
+    #         # print(count_2)
+    # if not is_found:
+    #     print("Записей не найдено")
+    # else:
+    #     if count_2 > 1: 
+    #         lit = "о"
+    #         lit_2 = "а"
+    #     else:
+    #         lit = ""
+    #         lit_2 = ""
+    #     print(f'Найден{lit} {count_2} элемент{lit_2}')
+    # if count_2 > 1:
+    #     print('Найдено несколько элементов, уточните параметры поиска через ";"')
+    #     filter_data()
+    # return element
     
-    print('5+')
+                   
+def change_data ():
+    delete_data()
+    input_data()
+
 
 def delete_data():
-    filter_string = input()
-    with open(file_name, 'r', encoding='utf-8') as file:
-        list_data = file.readlines()
-        # print(list_data)
-        if ";" in filter_string:
-            list_filter = filter_string.split(";")
-        else:
-            list_filter = [filter_string]
-
-        is_found = False
-        count_2 = 0
-        for element in list_data:
-            temp_record = element.split(";")
-            # print(temp_record)
-            count = 0
-            for record in temp_record:
-                for element_filter in list_filter:
-                    if element_filter.lower() in record.lower(): #and len(element_filter.lower()) == len(record.lower()):
-                        count += 1
-                        print(count, record)
-            if count >= len(list_filter):
-                print(element)
-                count_2 += 1
-                is_found = True
-            # print(count_2)
-    if not is_found:
-        print("Записей не найдено")
-    else:
-        if count_2 > 1: 
-            lit = "о"
-            lit_2 = "а"
-        else:
-            lit = ""
-            lit_2 = ""
-        print(f'Найден{lit} {count_2} элемент{lit_2}')
-    if count_2 > 1:
-        print('Уточните параметры')
-        delete_data()
-    d = str(input('Удалить этот элемент "Д/Н" ?'))
-    if d.lower() == "д":
-        print(temp_record, element, record)
-        # temp_record.clear()
+    print(filter_data())
+    # filter_string = input()
+    # with open(file_name, 'r', encoding='utf-8') as file:
+    #     list_data = file.readlines()
+    #     with open(file_name, 'w', encoding='utf-8') as fw:
+    #         for line in list_data:
+    #             if line.find(filter_string) == -1:
+    #                 fw.write(line)
